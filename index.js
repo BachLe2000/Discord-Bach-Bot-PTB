3925576aaea77fe0c9234bf384e7d31b9dd3d3ad
Discord Bach Le <65167922+BachLe2000@users.noreply.github.com>
2021-10-11 10:49:44 +0000

    Delete level feature.

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const keepAlive = require ("./server")
 
 const fs = require('fs');
 
-const xpfile = require('./xp.json')
-
 Client.on("ready", async () => {
     console.log(`${Client.user.username} đang hoạt động!`)
 
@@ -43,53 +41,6 @@ Client.on("message", async message => {
 
 })
 
-Client.on("message" ,function(message) {
-    if(message.author.Client) return;
-    var addXP = Math.floor(Math.random() * 10); 
-
-    if(!xpfile[message.author.id]) {
-        xpfile[message.author.id] = {
-           xp: 0,
-           level: 1,
-           reqxp: 100
-        }
-
-       fs.writeFile("./xp.json",JSON.stringify(xpfile),function(err){ 
-        if(err) console.log(err)
-       })
-    }
-
-    xpfile[message.author.id].xp += addXP
-
-    if(xpfile[message.author.id].xp > xpfile[message.author.id].reqxp){
-        xpfile[message.author.id].xp -= xpfile[message.author.id].reqxp 
-        xpfile[message.author.id].reqxp *= 2 
-        xpfile[message.author.id].reqxp = Math.floor(xpfile[message.author.id].reqxp) 
-        xpfile[message.author.id].level += 1 
-     
-        message.reply("You Are Now Level **"+xpfile[message.author.id].level+"**!").then( 
-            msg=>msg.delete({timeout: "10000"})
-        )
-
-    }
-
-    fs.writeFile("./xp.json",JSON.stringify(xpfile),function(err){
-        if(err) console/log(err)
-    })
-
-    if(message.content.startsWith("p=level")){
-        let user = message.mentions.users.first() || message.author
-
-        let embed = new Discord.MessageEmbed()
-        .setTitle("Level Card")
-        .setColor("GREEN")
-        .addField("Level: ",xpfile[user.id].level)
-        .addField("XP: ", xpfile[user.id].xp+"/"+xpfile[user.id].reqxp)
-        .addField("XP Required: ",xpfile[user.id].reqxp)
-        message.channel.send(embed)
-    }
-}) 
-
 Client.on("message", async message => {
     if(message.author.Client || message.channel.type === "dm") return;
 
@@ -104,8 +55,8 @@ Client.on("message", async message => {
         let embed = new Discord.MessageEmbed()
         .setTitle("Change Log")
         .setColor("RED")
-        .addField("Version:", "ALPHA 1.1")
-        .addField("What's new? ", "1.Add changelog command!                                  2.GitHub repo(Bot is now open source)")
+        .addField("Version:", "ALPHA 1.3")
+        .addField("What's new? ", "Delete level command")
         message.channel.send(embed)
     }
 })
